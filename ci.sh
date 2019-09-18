a2ca15dd76230a9753adbd6d762ffdbce010575b
rsp9u <gctwt852@yahoo.co.jp>
2019-09-19 02:49:00 +0900

    Rename document folder [skip ci]

--- ci.sh
+++ ci.sh
@@ -3,15 +3,15 @@ git checkout master
 
 # generate documents
 if [ "$1" = "apis" ]; then
-  mkdir -p doc/apis
+  mkdir -p docs/apis
   for f in $(find src/apis -name "*.yaml"); do
-    redoc-cli bundle $f -o doc/apis/$(basename $f .yaml).html
+    redoc-cli bundle $f -o docs/apis/$(basename $f .yaml).html
   done
   commit_msg="Update API documents [skip ci]"
 else
-  mkdir -p doc/umls
+  mkdir -p docs/umls
   for f in $(find src/umls -name "*.puml"); do
-    curl http://www.plantuml.com/plantuml/png/$(cat $f | python pte.py) -Lo doc/umls/$(basename $f .puml).png
+    curl http://www.plantuml.com/plantuml/png/$(cat $f | python pte.py) -Lo docs/umls/$(basename $f .puml).png
   done
   commit_msg="Update PlantUML documents [skip ci]"
 fi
@@ -20,7 +20,7 @@ fi
 echo "machine github.com"   > ~/.netrc
 echo "login $GIT_USER"     >> ~/.netrc
 echo "password $GIT_TOKEN" >> ~/.netrc
-git add doc -A
+git add docs -A
 git config user.name "Travis.CI"
 git config user.email "travis.auto.commit@localdomain"
 git commit -m "$commit_msg"
--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ yarn global add redoc-cli
 apk add python curl
 
 for f in $(find src/apis -name "*.yaml"); do
-  redoc-cli bundle $f -o doc/apis/$(basename $f .yaml).html
+  redoc-cli bundle $f -o docs/apis/$(basename $f .yaml).html
 done
 
 for f in $(find src/umls -name "*.puml"); do
-  curl http://www.plantuml.com/plantuml/png/$(cat $f | python pte.py) -Lo doc/umls/$(basename $f .puml).png
+  curl http://www.plantuml.com/plantuml/png/$(cat $f | python pte.py) -Lo docs/umls/$(basename $f .puml).png
 done
